c18638c386e56d6df277a81511dde51281f20902
davisjt <jaynsilentbob@gmail.com>
2009-09-19 18:47:55 -0400

    Updated tracker

--- src/ObjectTracker.java
+++ src/ObjectTracker.java
@@ -46,37 +46,6 @@ public class ObjectTracker implements Sink<ImageMoments>
 		g.setColor(c);
 		g.draw3DRect(150, 150, 40, 40, true);
 		//g.draw3DRect(moment.x, moment.y, moment.L1, moment.L2, true);
-		
-		//frame.getRawImage().createGraphics().draw3DRect(moment.x, moment.y, moment.L1, moment.L2, true);
-		
-		/*Pair<Integer, Integer> centroid = new Pair<Integer, Integer>(moment.x, moment.y);
-		
-		int bottomx = ((centroid.getFirst() + moment.L1) > frame.height()) ? frame.height() : centroid.getFirst() + moment.L1;
-		int topx = ((centroid.getFirst() - moment.L1) < 0) ? 0 : centroid.getFirst() - moment.L1;
-		int lefty = ((centroid.getSecond() - moment.L2) < 0) ? 0 : centroid.getSecond() - moment.L2;
-		int righty = ((centroid.getSecond() + moment.L2) > frame.width()) ? frame.width() : centroid.getSecond() + moment.L2;
-		
-		results.updateText("Bounding Box");
-		results.updateText("Top X: " + topx + ", Bottom X: " + bottomx + ", Left Y:" + lefty + ", Right Y:" + righty);
-		
-		// Draw box
-		for (int i = 0; i <= (moment.L2 * 2); i++)
-		{
-			if ((lefty + i) > frame.width())
-				break;
-			
-			frame.set(topx, lefty + i, box);
-			frame.set(bottomx, lefty + i, box);
-		}
-		
-		for (int i = 0; i < (moment.L1 * 2); i++)
-		{
-			if ((topx + i) > frame.height())
-				break;
-			
-			frame.set(topx + i, lefty, box);
-			frame.set(topx + i, righty, box);
-		}*/
 	}
 	
 	public CS440Image GetTrackedFrame()
