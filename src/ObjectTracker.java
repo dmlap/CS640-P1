8a4ad759b099bb65484e17082720d44622a6cdfb
davisjt <jaynsilentbob@gmail.com>
2009-09-19 18:02:29 -0400

    Changed bounding box

--- src/ObjectTracker.java
+++ src/ObjectTracker.java
@@ -1,4 +1,6 @@
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
 
 public class ObjectTracker implements Sink<ImageMoments>
@@ -37,10 +39,16 @@ public class ObjectTracker implements Sink<ImageMoments>
 	
 	public void DrawBoundingBox(ImageMoments moment)
 	{
-		// Set bounding box color
-		Color box = new Color(255, 255, 255);
+		// Set bounding box
+		Color c = new Color(255, 255, 255);
+		BufferedImage img = frame.getRawImage();
+		Graphics g = img.createGraphics();
+		g.setColor(c);
+		g.draw3DRect(moment.x, moment.y, moment.L1, moment.L2, true);
 		
-		Pair<Integer, Integer> centroid = new Pair<Integer, Integer>(moment.x, moment.y);
+		//frame.getRawImage().createGraphics().draw3DRect(moment.x, moment.y, moment.L1, moment.L2, true);
+		
+		/*Pair<Integer, Integer> centroid = new Pair<Integer, Integer>(moment.x, moment.y);
 		
 		int bottomx = ((centroid.getFirst() + moment.L1) > frame.height()) ? frame.height() : centroid.getFirst() + moment.L1;
 		int topx = ((centroid.getFirst() - moment.L1) < 0) ? 0 : centroid.getFirst() - moment.L1;
@@ -67,7 +75,7 @@ public class ObjectTracker implements Sink<ImageMoments>
 			
 			frame.set(topx + i, lefty, box);
 			frame.set(topx + i, righty, box);
-		}
+		}*/
 	}
 	
 	public CS440Image GetTrackedFrame()
