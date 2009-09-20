698411e54eeb62240383abc0531a35e5b4561cbd
David LaPalomento <dml@eta.(none)>
2009-09-19 21:16:18 -0400

    additional test for image moments

--- src/ImageMomentTest.java
+++ src/ImageMomentTest.java
@@ -16,7 +16,7 @@ public class ImageMomentTest {
 	}
 
 	@Test
-	public void calc() {
+	public void checkerboard() {
 		BufferedImage i = new BufferedImage(2, 2, BufferedImage.TYPE_INT_RGB);
 		i.setRGB(0, 0, Color.WHITE.getRGB());
 		i.setRGB(1, 1, Color.WHITE.getRGB());
@@ -40,6 +40,33 @@ public class ImageMomentTest {
 		assertCloseTo(0.5D, im.x);
 		assertCloseTo(0.5D, im.y);
 		assertCloseTo(2.45D, im.L1);
+		assertCloseTo(0D, im.L2);
+	}
+	
+	@Test
+	public void upperLeft() {
+		BufferedImage i = new BufferedImage(2, 2, BufferedImage.TYPE_INT_RGB);
+		i.setRGB(0, 0, Color.WHITE.getRGB());
+		final List<ImageMoments> ims = new ArrayList<ImageMoments>();
+		ImageMomentsGenerator img = new ImageMomentsGenerator();
+		img.subscribe(new Sink<ImageMoments>() {
+			@Override
+			public void receive(ImageMoments im) {
+				ims.add(im);
+			}
+		});
+		img.receive(new CS440Image(i));
+		assertEquals(1, ims.size());
+		ImageMoments im = ims.get(0);
+		assertCloseTo(1.0D, im.m00);
+		assertCloseTo(0D, im.m01);
+		assertCloseTo(0D, im.m02);
+		assertCloseTo(0D, im.m10);
+		assertCloseTo(0D, im.m11);
+		assertCloseTo(0D, im.m20);
+		assertCloseTo(0D, im.x);
+		assertCloseTo(0D, im.y);
+		assertCloseTo(0D, im.L1);
 		assertCloseTo(0D, im.L2);
 	}
 }
